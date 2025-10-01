eda86b88256e60b43c84bd0ded0dae8c5a0c7af8
Moon9t <luyanduthandokhumalo@gmail.com>
2025-10-01 16:35:24 +0200

    Currently developing the server

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/stripe/stripe-go/v83"
+	"github.com/stripe/stripe-go/v83/paymentintent"
 )
 
 func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
@@ -11,7 +15,51 @@ func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
 		return
 	}
-	fmt.Println("Payment Intent Created")
+	var req struct {
+		ProductID string `json:"product_id"`
+		FirstName string `json:"first_name"`
+		LastName  string `json:"last_name"`
+		Email     string `json:"email"`
+		Address1  string `json:"address1"`
+		Address2  string `json:"address2"`
+		City      string `json:"city"`
+		State     string `json:"state"`
+		Zip       string `json:"zip"`
+		Country   string `json:"country"`
+	}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(calculateOrderAmount(req.ProductID)),
+		Currency: stripe.String(string(stripe.CurrencyZAR)),
+		AutomaticPaymentMethods: &stripe.PaymentIntentAutomaticPaymentMethodsParams{
+			Enabled: stripe.Bool(true),
+		},
+	}
+
+	paymentIntent, err := paymentintent.New(params)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(paymentIntent.ClientSecret)
+}
+
+func calculateOrderAmount(productID string) int64 {
+	switch productID {
+	case "Pannts":
+		return 1000
+	case "T-Shirt":
+		return 2000
+	case "Flops":
+		return 3000
+	default:
+		return 0
+	}
 }
 
 func health(w http.ResponseWriter, r *http.Request) {
@@ -23,6 +71,7 @@ func health(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	stripe.Key = "sk_test_51PfX52GfpWuhLYr22YKzuDJNLfrZgkpmzq1kJAVCZMRfOW6JCPj8nmYRECP3K95vPtSLwwDQYZXbGowCpg1gbRiy00aRQmZgzC"
 	http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
 	http.HandleFunc("/health", health)
 	log.Println("Listening on localhost:4242...")
